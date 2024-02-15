c2917a47aa3ec75728f6fe14552ed0c2d7d12457
Saransh <saranshtomar926@gmail.com>
2024-02-15 12:41:04 +0530

    inital commit

---
+++ src/main/java/com/saransh/sprinbootapi/SpringbootCrudApiH2/SpringbootCrudApiH2Application.java
@@ -0,0 +1,13 @@
+package com.saransh.sprinbootapi.SpringbootCrudApiH2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudApiH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudApiH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/saransh/sprinbootapi/SpringbootCrudApiH2/SpringbootCrudApiH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saransh.sprinbootapi.SpringbootCrudApiH2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudApiH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
